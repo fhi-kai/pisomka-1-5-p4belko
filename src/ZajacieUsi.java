9fe5b9ac1cbcaa2ba5a50cb83bf7bcc552d5e2d9
Peter Belko <p4belko@gmail.com>
2018-03-07 17:50:24 +0100

    solution

--- src/ZajacieUsi.java
+++ src/ZajacieUsi.java
@@ -12,3 +12,19 @@ public interface ZajacieUsi {
 	public int zajacieUsi(int pocetZajacov);
 
 }
+
+class MojeUsi implements ZajacieUsi{
+
+	@Override
+	public int zajacieUsi(int pocetZajacov) {
+
+		if (pocetZajacov == 0){
+			return 0;
+		}else if (pocetZajacov % 2 == 0){
+			return 3 + zajacieUsi(pocetZajacov -1);
+		}else{
+			return 2 + zajacieUsi(pocetZajacov -1);
+		}
+		//(pocetZajacov % 2 != 0)
+	}
+}
